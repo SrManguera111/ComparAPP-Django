1a0f70a5533d306a1974580e78a8c29e44d2fba2
Benjamin <10benja.santis@gmail.com>
2025-12-08 19:50:28 -0300

    Configuracion de correos reales con Gmail

--- ComparAPP_WEB/urls.py
+++ ComparAPP_WEB/urls.py
@@ -3,41 +3,43 @@ from . import views
 from django.contrib.auth import views as auth_views
 
 urlpatterns = [
+    # --- VISTAS PRINCIPALES ---
     path('', views.Index, name='index'),
+    path('nosotros/', views.nosotros, name='nosotros'),
+    path('productos/', views.catalogo_completo, name='catalogo'),
+    
+    # --- USUARIOS ---
     path('login/', views.iniciar_sesion, name='iniciar_sesion'),
     path('registro/', views.crear_cuenta, name='crear_cuenta'),
-    path('nosotros/', views.nosotros, name='nosotros'),
     path('logout/', views.cerrar_sesion, name='cerrar_sesion'),
+    path('perfil/', views.perfil, name='perfil'),
+    
+    # --- PROCESO DE COMPRA ---
+    path('procesar_orden/', views.procesar_orden, name='procesar_orden'),
+    path('historial/', views.historial, name='historial'),
+    path('buscar/', views.buscar, name='buscar'),
 
-    # Formulario para pedir el correo
+   
+    # RECUPERACIÓN DE CONTRASEÑA (GMAIL)
+    
+
+    # Formulario donde el usuario pone su correo
     path('reset_password/', 
          auth_views.PasswordResetView.as_view(template_name="password_reset.html"), 
          name='password_reset'),
 
-    # Mensaje de "Correo enviado"
+    # Mensaje de éxito: "Correo enviado, revisa tu bandeja"
     path('reset_password_sent/', 
-        auth_views.PasswordResetDoneView.as_view(template_name="password_reset_sent.html"), 
-        name='password_reset_done'),
+         auth_views.PasswordResetDoneView.as_view(template_name="password_reset_sent.html"), 
+         name='password_reset_done'),
 
-    #  Link que llega al correo (para poner la nueva clave)
+    # Link que llega al correo -> Formulario para poner la nueva clave
     path('reset/<uidb64>/<token>/', 
-        auth_views.PasswordResetConfirmView.as_view(template_name="password_reset_form.html"), 
-        name='password_reset_confirm'),
+         auth_views.PasswordResetConfirmView.as_view(template_name="password_reset_form.html"), 
+         name='password_reset_confirm'),
 
-    # Mensaje de "Contraseña cambiada con éxito"
+    # Mensaje final: "¡Listo! Contraseña cambiada"
     path('reset_password_complete/', 
-        auth_views.PasswordResetCompleteView.as_view(template_name="password_reset_done.html"), 
-        name='password_reset_complete'),
-        
-    path('procesar_orden/', views.procesar_orden, name='procesar_orden'),
-
-    path('historial/', views.historial, name='historial'),
-
-    path('buscar/', views.buscar, name='buscar'),
-
-    path('perfil/', views.perfil, name='perfil'),
-
-    path('productos/', views.catalogo_completo, name='catalogo'),
+         auth_views.PasswordResetCompleteView.as_view(template_name="password_reset_complete.html"), 
+         name='password_reset_complete'),
 ]
-
-
