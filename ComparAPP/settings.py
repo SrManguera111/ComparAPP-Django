65575a1a85a68748db6d495445eef61308e9f259
Benjamin <10benja.santis@gmail.com>
2025-12-08 20:31:10 -0300

    Cambio a puerto 587 SSL para arreglar timeout de correo

--- ComparAPP/settings.py
+++ ComparAPP/settings.py
@@ -143,14 +143,14 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 
 # Configuración del servidor de Gmail
-#EMAIL_HOST = 'smtp.gmail.com'
-#EMAIL_PORT = 465
-#EMAIL_USE_TLS = True
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
 
-#EMAIL_HOST_USER = '10benja.santis@gmail.com' 
+EMAIL_HOST_USER = '10benja.santis@gmail.com' 
 
-#EMAIL_HOST_PASSWORD = 'qpxk slew cajp jdbq' 
+EMAIL_HOST_PASSWORD = 'qpxk slew cajp jdbq' 
 
-#DEFAULT_FROM_EMAIL = 'ComparAPP <10benja.santis@gmail.com>'#
+DEFAULT_FROM_EMAIL = 'ComparAPP <10benja.santis@gmail.com>'
 
-EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+
