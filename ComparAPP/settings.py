f9c887c4f72a4bd0aab6ab79dffbcff3fc7491da
Benjamin <10benja.santis@gmail.com>
2025-12-08 20:04:42 -0300

    Cambio a puerto 465 SSL para arreglar timeout de correo

--- ComparAPP/settings.py
+++ ComparAPP/settings.py
@@ -144,7 +144,7 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 
 # Configuración del servidor de Gmail
 EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_PORT = 587
+EMAIL_PORT = 465
 EMAIL_USE_TLS = True
 
 EMAIL_HOST_USER = '10benja.santis@gmail.com' 
