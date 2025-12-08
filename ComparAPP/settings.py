fdd1d3cbcbfa7e35c9159ced14e1fb28f91c1d3d
Benjamin <10benja.santis@gmail.com>
2025-12-08 19:35:29 -0300

    Configuracion de correos reales con Gmail

--- ComparAPP/settings.py
+++ ComparAPP/settings.py
@@ -136,5 +136,19 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
-# Configuración de correos para desarrollo (La consola)
-EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+# ==========================================
+# CONFIGURACIÓN DE CORREO (GMAIL)
+# ==========================================
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+
+# Configuración del servidor de Gmail
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+
+EMAIL_HOST_USER = '10benja.santis@gmail.com' 
+
+EMAIL_HOST_PASSWORD = 'qpxk slew cajp jdbq' 
+
+DEFAULT_FROM_EMAIL = 'ComparAPP <10benja.santis@gmail.com>'
